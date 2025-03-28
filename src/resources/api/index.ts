32199d162c140315ed4bc7ef8114ecd8a4da194e
Rafal Mikucki <rafalmikucki@gmail.com>
2025-03-28 12:18:53 +0100

    hotfix: build

--- src/resources/api/index.ts
+++ src/resources/api/index.ts
@@ -1,14 +1,6 @@
 // File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
-
-export { API } from './api/index';
 export {
   API,
-  type APIConnectAuthResponse,
-  type APICreateAssetResponse,
-  type APIListAuctionsResponse,
-  type APIConnectAuthParams,
-  type APICreateAssetParams,
-  type APIListAuctionBidsParams,
 } from './api';
 export {
   Loyalty,
